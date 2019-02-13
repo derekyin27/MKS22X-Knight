dd1744c091860b1254fe63ded09a0ed8046aac2c
Derek <dyin00@stuy.edu>
2019-02-13 11:41:33 -0500

    working on solveH

--- KnightBoard.java
+++ KnightBoard.java
@@ -16,8 +16,11 @@ public String toString(){
   for (int r =0; r < board.length; r++){
     for (int c=0; c < board[0].length; c++){
       if (board[r][c] == 0){
-        str+= "0 ";
+        str+= "_ ";
+      }else{
+        str+= board[r][c] + " ";
       }
+
     }
     str+="\n";
   }
@@ -31,7 +34,7 @@ public boolean solve(int startingRow, int startingCol){
         throw new IllegalStateException();
     }
   }
-  if (startingRow <= 0 || startingRow >= board.length || startingCol <= 0 || startingCol >= board[0].length){
+  if (startingRow < 0 || startingRow >= board.length || startingCol < 0 || startingCol >= board[0].length){
     throw new IllegalArgumentException();
   }else {
     return solveH(startingRow, startingCol, 1);
@@ -42,11 +45,11 @@ private boolean solveH(int row, int col, int level){
   if (level == board.length * board[0].length){
     return true;
   }
-  if (row >= board.length || col >= board[0].length){
+  if (row >= board.length || col >= board[0].length || row <0 || col<0){
     return false;
   }
   else{
-    if (board[row][col] == 0)
+    if (board[row][col] == 0){
     board[row][col] = level;
     return solveH(row+1, col+2, level+1) ||
     solveH(row+1, col-2, level+1) ||
@@ -57,10 +60,12 @@ private boolean solveH(int row, int col, int level){
     solveH(row-2, col+1, level+1) ||
     solveH(row-2, col-1, level+1);
   }
+  return true;
+  }
 }
 public static void main(String[] args) {
   KnightBoard test = new KnightBoard(7, 7);
-  System.out.println(test);
   System.out.println(test.solve(0,0));
+  System.out.println(test);
 }
 }
